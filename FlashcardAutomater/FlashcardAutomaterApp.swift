d6d0869b108c4a2c3d08767ab72b729bf945d219
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2023-07-17 14:24:38 +0800

    Initial Commit

---
+++ FlashcardAutomater/FlashcardAutomaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardAutomaterApp.swift
+//  FlashcardAutomater
+//
+//  Created by Leon on 2023/7/17.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardAutomaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
