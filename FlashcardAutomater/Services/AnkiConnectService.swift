f24adab06725d7a38543b88dd09cfca255cd6d26
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2023-08-07 14:57:56 +0800

    Add stubs for AnkiConnect

---
+++ FlashcardAutomater/Services/AnkiConnectService.swift
@@ -0,0 +1,16 @@
+//
+//  AnkiConnectService.swift
+//  FlashcardAutomater
+//
+//  Created by Leon on 2023/8/7.
+//
+
+import Foundation
+
+struct AnkiConnectService {
+    
+}
+
+extension AnkiConnectService: AnkiConnectServiceProtocol {
+    
+}
---
+++ FlashcardAutomater/Services/AnkiConnectServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AnkiConnectServiceProtocol.swift
+//  FlashcardAutomater
+//
+//  Created by Leon on 2023/8/7.
+//
+
+import Foundation
+
+protocol AnkiConnectServiceProtocol {
+    
+}
